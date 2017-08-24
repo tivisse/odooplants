4ce147d8264499c4eeec2f5af5b9908e67b01a92
Yannick Tivisse <yti@odoo.com>
2017-08-24 10:07:20 +0200

    [ADD] plant_nursery: New plant management module
    The manifest is the minimal content for an Odoo module
    No file and demo to import at the moment

---
+++ README.md
@@ -0,0 +1,10 @@
+Plant Nursery Module
+====================
+
+This module is a simple usecase to present the Odoo framework during the
+Odoodays 2018.
+
+See the [commit history](https://github.com/tivisse/odoodays-2018/commits/master/)
+to follow the evolution of the module.
+
+See the full presentation at InsertLink
---
+++ plant_nursery/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Plant Nursery',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Plants and Customers Management',
+    'depends': ['web'],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'css': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
