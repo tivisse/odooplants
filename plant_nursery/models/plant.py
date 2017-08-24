d187c4ffb0f7f934b71b7bdcd36e0b98562d3f40
Yannick Tivisse <yti@odoo.com>
2017-08-24 10:23:07 +0200

    [IMP] plant_nursery: Introduce basic models

---
+++ plant_nursery/models/plant.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class Plants(models.Model):
+    _name = 'nursery.plant'
+    _description = 'Nursery Plant'
+
+    name = fields.Char("Plant Name")
+    price = fields.Float()
