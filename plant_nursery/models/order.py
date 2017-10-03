06853da03ea9adceb9699ec0b7868a400ee1d92e
Yannick Tivisse <yti@odoo.com>
2017-10-03 17:08:13 +0200

    [IMP] plant_nursery: Add ORM interactions

--- plant_nursery/models/order.py
+++ plant_nursery/models/order.py
@@ -2,10 +2,32 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import fields, models
+from odoo.exceptions import UserError
 
 
 class Order(models.Model):
     _name = 'nursery.order'
 
+    name = fields.Datetime(default=fields.Datetime.now)
     plant_id = fields.Many2one("nursery.plant", required=True)
     customer_id = fields.Many2one("nursery.customer")
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('confirm', 'Confirmed'),
+        ('cancel', 'Canceled')
+    ], default='draft')
+    last_modification = fields.Datetime(readonly=True)
+
+    def write(self, values):
+        # helper to "YYYY-MM-DD"
+        values['last_modification'] = fields.Datetime.now()
+
+        return super(Order, self).write(values)
+
+    def unlink(self):
+        # self is a recordset
+        for order in self:
+            if order.state == 'confirm':
+                raise UserError("You can not delete confirmed orders")
+
+        return super(Order, self).unlink()
