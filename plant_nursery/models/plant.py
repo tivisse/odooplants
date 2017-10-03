85470ac485255f9565e3fe47b6c44f7e9a59ce5e
Yannick Tivisse <yti@odoo.com>
2017-10-03 17:30:06 +0200

    [IMP] plant_nursery: Add compute field on plants

--- plant_nursery/models/plant.py
+++ plant_nursery/models/plant.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class Plants(models.Model):
@@ -10,3 +10,11 @@ class Plants(models.Model):
     name = fields.Char("Plant Name")
     price = fields.Float()
     order_ids = fields.One2many("nursery.order", "plant_id", string="Orders")
+    order_count = fields.Integer(compute='_compute_order_count',
+                                 store=True,
+                                 string="Total sold")
+
+    @api.depends('order_ids')
+    def _compute_order_count(self):
+        for plant in self:
+            plant.order_count = len(plant.order_ids)
