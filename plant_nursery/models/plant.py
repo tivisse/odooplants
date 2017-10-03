2964fdcc3db4b187edd9264c3e6950d09952b09b
Yannick Tivisse <yti@odoo.com>
2017-10-03 17:33:34 +0200

    [IMP] plant_nursery: Add constraint on plants

--- plant_nursery/models/plant.py
+++ plant_nursery/models/plant.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
+from odoo.exceptions import UserError
 
 
 class Plants(models.Model):
@@ -13,8 +14,17 @@ class Plants(models.Model):
     order_count = fields.Integer(compute='_compute_order_count',
                                  store=True,
                                  string="Total sold")
+    number_in_stock = fields.Integer()
 
     @api.depends('order_ids')
     def _compute_order_count(self):
         for plant in self:
             plant.order_count = len(plant.order_ids)
+
+    @api.constrains('order_count', 'number_in_stock')
+    def _check_available_in_stock(self):
+        for plant in self:
+            if plant.number_in_stock and \
+              plant.order_count > plant.number_in_stock:
+                raise UserError("There is only %s %s in stock but %s were sold"
+                      % (plant.number_in_stock, plant.name, plant.order_count))
