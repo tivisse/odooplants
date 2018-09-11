ffdf749ce6ccd8266259e9b3ccf1ce67a6f2eebe
Yannick Tivisse <yti@odoo.com>
2018-09-11 14:17:46 +0200

    [ADD] plant_nursery_data: Add data module for plants

---
+++ plant_nursery_data/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Plant Nursery Data',
+    'version': '1.0',
+    'category': 'Hidden',
+    'summary': 'Data for Plant Nursery',
+    'depends': ['plant_nursery', 'mail'],
+    'data': [
+        'data/base_data.xml',
+        'data/customer_data.xml',
+        'data/plant_data.xml',
+        'data/order_data.xml',
+    ],
+    'demo': [
+    ],
+    'css': [],
+    'auto_install': False,
+    'application': False,
+}
