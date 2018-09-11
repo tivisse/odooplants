a94d8c490cf63831aaccf2cba4ea4f950338e49d
Yannick Tivisse <yti@odoo.com>
2018-09-11 13:18:51 +0200

    [IMP] plant_nursery: Improve plants models and views
    - Add tags and categories models on plants
    - Add some useful fields on plants model
    - Add kanban,tree,form views for the plants
    - Add views and menuitems for tags and categories

--- plant_nursery/models/plant.py
+++ plant_nursery/models/plant.py
@@ -1,31 +1,54 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 from odoo.exceptions import UserError
+
+
+class Category(models.Model):
+    _name = 'nursery.plant.category'
+    _description = 'Plant Category'
+    _order = 'name'
+
+    name = fields.Char('Name', required=True)
+
+
+class Tag(models.Model):
+    _name = 'nursery.plant.tag'
+    _description = 'Plant Tag'
+    _order = 'name'
+
+    name = fields.Char('Name', required=True)
+    color = fields.Integer('Color Index', default=10)
 
 
 class Plants(models.Model):
     _name = 'nursery.plant'
 
-    name = fields.Char("Plant Name")
+    name = fields.Char("Plant Name", required=True)
     price = fields.Float()
+    description_short = fields.Html('Short description')
+    description = fields.Html('Description')
+    category_id = fields.Many2one('nursery.plant.category', string='Category')
+    tag_ids = fields.Many2many('nursery.plant.tag', string='Tags')
     order_ids = fields.One2many("nursery.order", "plant_id", string="Orders")
     order_count = fields.Integer(compute='_compute_order_count',
                                  store=True,
                                  string="Total sold")
     number_in_stock = fields.Integer()
     image = fields.Binary("Plant Image", attachment=True)
+    user_id = fields.Many2one(
+        'res.users', string='Responsible',
+        index=True, required=True,
+        default=lambda self: self.env.user)
 
     @api.depends('order_ids')
     def _compute_order_count(self):
         for plant in self:
             plant.order_count = len(plant.order_ids)
 
-    @api.constrains('order_count', 'number_in_stock')
+    @api.constrains('number_in_stock')
     def _check_available_in_stock(self):
         for plant in self:
-            if plant.number_in_stock and \
-              plant.order_count > plant.number_in_stock:
-                raise UserError("There is only %s %s in stock but %s were sold"
-                      % (plant.number_in_stock, plant.name, plant.order_count))
+            if plant.number_in_stock < 0:
+                raise UserError(_('Stock cannot be negative.'))
