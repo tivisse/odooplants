8adda2fa091c833df9d8bd8c55c0cc33368ab957
Yannick Tivisse <yti@odoo.com>
2018-09-13 13:17:04 +0200

    [IMP] plant_nursery: Add website controllers to test the utm.mixin

--- plant_nursery/__manifest__.py
+++ plant_nursery/__manifest__.py
@@ -6,7 +6,7 @@
     'version': '1.0',
     'category': 'Tools',
     'summary': 'Plants and Customers Management',
-    'depends': ['web', 'mail', 'sms', 'documents', 'rating'],
+    'depends': ['web', 'mail', 'sms', 'documents', 'rating', 'website'],
     'data': [
         'security/ir.model.access.csv',
         'views/assets.xml',
@@ -15,6 +15,8 @@
         'views/nursery_customer_views.xml',
         'views/nursery_order_views.xml',
         'views/rating_rating_views.xml',
+        'views/nursery_plant_templates.xml',
+        'views/nursery_plant_quote_ask.xml',
         'data/ir_sequence_data.xml',
         'data/mail_template_data.xml',
         'data/documents_data.xml',
