1852f8f6bba2e75bfbb333fe0952043c1d35f698
Yannick Tivisse <yti@odoo.com>
2018-09-13 13:17:04 +0200

    [IMP] plant_nursery: Add website controllers to test the utm.mixin

--- plant_nursery/models/customer.py
+++ plant_nursery/models/customer.py
@@ -15,3 +15,13 @@ class Customer(models.Model):
     address = fields.Char('Address')
     country_id = fields.Many2one('res.country', string='Country')
     partner_id = fields.Many2one('res.partner', string="Company")
+
+    def find_or_create(self, name, email):
+        customer = self.search(['|', ('name', '=', name), ('email', '=', email)])
+        if not customer:
+            customer = self.create({'name': name, 'email': email})
+        elif customer.name != name:
+            customer.write({'name': name})
+        elif customer.email != email:
+            customer.write({'email': email})
+        return customer
